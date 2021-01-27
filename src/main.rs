973a003e34677e32bfba32c823bc3a049176bf3d
Purple Hair Rust Bard <AshantiMutinta@gmail.com>
2021-01-27 10:20:18 -0500

    using stream consumer

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,26 @@
-use rdkafka::{Message,ClientConfig};
-use rdkafka::consumer::BaseConsumer;
-use std::time::Duration;
+use futures::stream::StreamExt;
 use rdkafka::consumer::Consumer;
+use rdkafka::consumer::StreamConsumer;
+use rdkafka::{ClientConfig, Message};
+
 #[tokio::main]
 async fn main() {
-
-    
-    let consumer: BaseConsumer = ClientConfig::new()
-    .set("bootstrap.servers", "localhost:9092")
-    .set("group.id","Hopin")
-    .create()
-    .expect("Producer creation error");
+    let consumer: StreamConsumer = ClientConfig::new()
+        .set("bootstrap.servers", "localhost:9092")
+        .set("group.id", "Hopin")
+        .create()
+        .expect("Producer creation error");
 
     consumer.subscribe(&["hopin-live"]).unwrap();
-    loop 
-    {
-        match consumer.poll(Duration::from_secs(1))
-        {
-            Some(message_type) =>
-            {
-                match message_type
-                {
-                    Ok(message) =>
-                    {
-                        let message = message.detach().payload().map(|s|String::from_utf8(s.to_vec()).unwrap()).unwrap_or_default();
-                       println!("obtained{:?}",message);
-                    },
-                    Err(error_type) => println!("error obtainign message {:?}",error_type)
-                }
-            },
-            None => ()
+    let mut message_stream = consumer.start();
+    loop {
+        if let Some(Ok(message)) = message_stream.next().await {
+            let message = message
+                .detach()
+                .payload()
+                .map(|s| String::from_utf8(s.to_vec()).unwrap())
+                .unwrap_or_default();
+            println!("message obtained {:?}", message);
         }
-
     }
 }
